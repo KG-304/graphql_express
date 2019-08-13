7b34787c9ae618f21bd8e8aa1026adee1303220c
kgall <karl.forrest.gall@ibm.com>
2019-08-12 21:16:00 -0400

    store data in json before using cloudant.

--- server2.js
+++ server2.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var express_graphql = require('express-graphql');
 var { buildSchema } = require('graphql');
+var OpenMicsData = require('./OpenMicsData.json');
 const cors = require('cors')
 
 // GraphQL schema
@@ -22,53 +23,24 @@ var schema = buildSchema(`
         phone: String
     }
 `);
-var OpenMicsData = [
-    {
-        id: 1,
-        location: '27-16 23rd Avenue Astoria, NY 11105',
-        title: 'Open mic @ Q.E.D',
-        author: 'Hosted by: Chris Farley',
-        description: 'Be sure to come out and support your local comedians trying to make it big!',
-        url: 'https://qedastoria.com/',
-        phone: '347-451-3873'
-    },
-    {
-        id: 2,
-        location: '10-93 JACKSON AVENUE',
-        title: 'Open mic @ Creek and Cabin',
-        author: 'Hosted By: Adam Sandler',
-        description: 'Come play games and support your local comedians in LIC!',
-        url: 'https://creeklic.com/',
-        phone: '718-706-8783'
-    },
-    {
-        id: '3',
-        location: '307 W 26th Street New York, NY 10001 ',
-        title: 'Open mic @ Improv Asylum',
-        author: 'Hosted By: Tom Segura',
-        description: 'Come hang out with us tonight at our very own open mic!',
-        url: 'https://www.improvasylum.com/',
-        phone: '212-203-5435'
-    }
-]
 
 var getOpenMic = function(args) { 
     var id = args.id;
-    return OpenMicsData.filter(OpenMic => {
+    return OpenMicsData.data.filter(OpenMic => {
         return OpenMic.id == id;
     })[0];
 }
 var getOpenMics = function(args) {
-        return OpenMicsData;
+        return OpenMicsData.data;
 }
 var updateOpenMicLocation = function({id, location}) {
-    OpenMicsData.map(OpenMic => {
+    OpenMicsData.data.map(OpenMic => {
         if (OpenMic.id === id) {
             OpenMic.location = location;
             return OpenMic;
         }
     });
-    return OpenMicsData.filter(OpenMic => OpenMic.id === id) [0];
+    return OpenMicsData.data.filter(OpenMic => OpenMic.id === id) [0];
 }
 var root = {
     OpenMic: getOpenMic,
