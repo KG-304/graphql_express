ec5b09c3043518d7fc921dd7caadba1b583035de
kgall <karl.forrest.gall@ibm.com>
2019-08-21 12:53:31 -0400

    user api still not functional

--- server2.js
+++ server2.js
@@ -2,6 +2,7 @@ var express = require('express');
 var express_graphql = require('express-graphql');
 var { buildSchema } = require('graphql');
 var OpenMicsData = require('./OpenMicsData.json');
+var User = require('./user.json')
 const cors = require('cors')
 
 // GraphQL schema
@@ -9,6 +10,7 @@ var schema = buildSchema(`
     type Query {
         OpenMic(id: ID!): OpenMic
         OpenMics: [OpenMic]
+        getUser(id: ID!): User
     },
     type Mutation {
         updateOpenMicLocation(id: ID!, location: String!): OpenMic
@@ -22,7 +24,20 @@ var schema = buildSchema(`
         url: String
         phone: String
     }
+    type User{
+        id: ID!
+        email: String
+        firstName: String
+        lastName: String
+    }
 `);
+
+var getUser = function(args){
+    var id = args.id;
+    return User.data.filter(user => {
+        return user.id == id;
+    })[0];
+}
 
 var getOpenMic = function(args) { 
     var id = args.id;
@@ -30,9 +45,11 @@ var getOpenMic = function(args) {
         return OpenMic.id == id;
     })[0];
 }
+
 var getOpenMics = function(args) {
         return OpenMicsData.data;
 }
+
 var updateOpenMicLocation = function({id, location}) {
     OpenMicsData.data.map(OpenMic => {
         if (OpenMic.id === id) {
@@ -42,12 +59,14 @@ var updateOpenMicLocation = function({id, location}) {
     });
     return OpenMicsData.data.filter(OpenMic => OpenMic.id === id) [0];
 }
+
 var root = {
+    User: getUser,
     OpenMic: getOpenMic,
     OpenMics: getOpenMics,
     updateOpenMicLocation: updateOpenMicLocation
 };
-// Create an express server and a GraphQL endpoint
+
 var app = express();
 app.use(cors())
 app.use('/graphql', express_graphql({
